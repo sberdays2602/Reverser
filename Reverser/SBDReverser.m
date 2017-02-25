21d8b83e0d6a62a2f6fc7cd2bfe43a13f7119ddd
Alexey Ulenkov <alexeyulenkov@MacBook-Pro-Alexey-2.local>
2017-02-25 22:26:33 +0300

    Initial commit

---
+++ Reverser/SBDReverser.m
@@ -0,0 +1,30 @@
+//
+//  SBDReverser.m
+//  Reverser
+//
+//  Created by Alexey Ulenkov on 25.02.17.
+//  Copyright © 2017 Alexey Ulenkov. All rights reserved.
+//
+
+#import "SBDReverser.h"
+
+@implementation SBDReverser
+
++ (nullable NSString *)reverseString:(nullable NSString *)source {
+
+    if (nil == source)
+        return nil;
+        
+    NSMutableString *reversedString = [[NSMutableString alloc] initWithCapacity: source.length];
+    NSUInteger characterIndex = source.length;
+    NSUInteger incrementLenght = 1;
+    while (characterIndex > 0) {
+        --characterIndex;
+        NSRange iterationIncrement = NSMakeRange(characterIndex, incrementLenght);
+        [reversedString appendString:[source substringWithRange:iterationIncrement]];
+    }
+    return [reversedString copy];
+}
+
+@end
+
